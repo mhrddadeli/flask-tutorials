1e10ff5e1173fca34275f183eb664e06955ad40e
mhrddadeli <mhrddadeli@gmail.com>
2024-08-23 14:55:33 +0330

    auth featurs added

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,35 +1,22 @@
-from flask import Flaskforms
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Lenght, Email, EQualTo
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-class RegistrationForm(Flaskforms):
+class RegistrationForm(FlaskForm):
     username = StringField(
-        'username',
-        validators=[
-            DataRequired(),
-            Length(min=6, max=20)
-            ]
-        )
-    email = StringField(
-        'email',
-        validators=[
-            DataRequired(),
-            Email()
-            ]
-        ) 
-    password = PasswordField(
-        'password',
-        validators=[
-            DataRequired()
-            ]
+        "username", validators=[DataRequired(), Length(min=6, max=20)]
     )
+    email = StringField("email", validators=[DataRequired(), Email()])
+    password = PasswordField("password", validators=[DataRequired()])
     confirm_password = PasswordField(
-        'confirm password',
-        validators=[
-            DataRequired(),
-            EqualTo('password')
-            
-            ]
+        "confirm password", validators=[DataRequired(), EqualTo("password")]
     )
-    
+    submit = SubmitField("Sign Up")
+
+
+class LogInForm(FlaskForm):
+    email = StringField("email", validators=[DataRequired(), Email()])
+    password = PasswordField("password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Log In")
--- flask_blog/main.py
+++ flask_blog/main.py
@@ -1,34 +1,60 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LogInForm
 
-app = Flask(__name__) # __name__ is a special variable name of the module
 
+app = Flask(__name__)  # __name__ is a special variable name of the module
+app.config["SECRET_KEY"] = "8a3a6b5c3e912fc5022cdcbad9a6ade2"
 posts = [
     {
-        'title': 'Post 1',
-        'content': 'This is the first post',
-        'author': 'John Doe',
-        'date_posted': 'April 1, 2016',
+        "title": "Post 1",
+        "content": "This is the first post",
+        "author": "John Doe",
+        "date_posted": "April 1, 2016",
     },
     {
-        'title': 'Post 2',
-        'content': 'This is the second post',
-        'author': 'jackson Doe',
-        'date_posted': 'April 1, 2016',
+        "title": "Post 2",
+        "content": "This is the second post",
+        "author": "jackson Doe",
+        "date_posted": "April 1, 2016",
     },
-        
 ]
 
 
-@app.route("/") # decorators add extra functionality to an existing function
+@app.route("/")  # decorators add extra functionality to an existing function
 @app.route("/home")
 def home_page():
-    return render_template('home.html', posts=posts)
+    return render_template("home.html", posts=posts)
+
 
 @app.route("/about")
 def about_page():
-    return render_template('about.html', title='About Us')
+    return render_template("about.html", title="About Us")
 
 
-# run directy from the file 
-if __name__ == '__main__':
+@app.route("/register", methods=["GET", "POST"])
+def register_page():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(
+            f"Account Created for {form.username.data}!",
+            "success",
+        )
+        return redirect(url_for("home_page"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    form = LogInForm()
+    if form.validate_on_submit():
+        if form.email.data == "mhrddadeli@gmail.com" and form.password.data == "1234":
+            flash("You Are Logged In", "success")
+            return redirect(url_for("home_page"))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+    return render_template("login.html", title="Log In", form=form)
+
+
+# run directy from the file
+if __name__ == "__main__":
     app.run(debug=True)
